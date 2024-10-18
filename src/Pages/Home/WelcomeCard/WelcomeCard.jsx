05b61526c835d41a585319cebd6716f7c8fdcb92
oswaldo <oswaldo32gonzalez@gmail.com>
2024-10-17 21:32:53 -0600

    fix/ change align items for the welcome card and add more details to the message

--- src/Pages/Home/WelcomeCard/WelcomeCard.jsx
+++ src/Pages/Home/WelcomeCard/WelcomeCard.jsx
@@ -6,7 +6,7 @@ function WelcomeCard() {
         <section className={styles.container}>
             <article>
               <h2>Welcome to the cool website!</h2>
-              <p>Welcome to The Odin Project: React made by Oswaldo Gonzalez. This website is created to practices all odin&apos;s projects in react</p>
+              <p>Welcome to The Odin Project: React made by Oswaldo Gonzalez. This website is created to practices all odin&apos;s projects in react and in a near Future serve as a portfolio for my Data Engineering and my Full Stack skills.</p>
               <button>More About Me</button>
             </article>
             <img className={styles.img} alt='Image of Oswaldo Gonzalez'></img>
