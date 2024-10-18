b725bba4b7cf41d1399bde41faa39925dcd95a87
oswaldo <oswaldo32gonzalez@gmail.com>
2024-10-17 21:21:45 -0600

    feat/ add Quote Card component and fix padding discrepancies with welcome card

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import WelcomeCard from "./WelcomeCard/WelcomeCard"
 import styles from './Home.module.css'
 import ProjectsCard from "./ProjectsCard/ProjectsCard"
+import QuoteCard from "./QuoteCard/QuoteCard"
 
 function Home() {
   return(
     <main className={styles.wrapContainer}>
       <WelcomeCard />
       <ProjectsCard/>
+      <QuoteCard quote={"\"Every line of code is an opportunity to create something amazing. Don't get discouraged by mistakes; they are part of the process of innovation.\""}/>
     </main>
   )
 }
