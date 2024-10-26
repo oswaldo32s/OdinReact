5e9e1167663d8e1cc6abcc17c65c2516dc3f596a
oswaldo <oswaldo32gonzalez@gmail.com>
2024-10-26 00:09:19 -0600

    feat: Add contact us page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,6 +1,40 @@
+import Form from "../../components/Form/Form"
+import styles from './Contact.module.css'
+
 function Contact() {
+  const contactFormData = [
+    {
+      id: 1,
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      isRequired: true,
+    },
+    {
+      id: 2,
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+      isRequired: true
+    },
+    {
+      id: 3,
+      name: 'message',
+      label: 'Message',
+      type: 'textarea',
+      isRequired: true
+    },
+  ]
   return (
-    <h2>Page in construction, please be patient you&apos;ll see something here in the future</h2>
+    <div className={styles.wrapContainer}>
+      <section className={styles.container}>
+        <div className={styles.formSection}>
+          <h2 className={styles.title}>Contact Us!</h2>
+          <Form dataArray={contactFormData}/>
+        </div>
+        <img src="/images/contact-us.jpg" alt="contact us image" className={styles.img}/>
+      </section>
+    </div>
   )
 }
 
