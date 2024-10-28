4d16bbf71b67bc2ed07e1a9773593b1845ead7cc
oswaldo <oswaldo32gonzalez@gmail.com>
2024-10-27 22:48:29 -0600

    Feat: add Error page, add image to game project and adjust project data

--- src/Pages/Projects/ProjectsCard/projectsData.js
+++ src/Pages/Projects/ProjectsCard/projectsData.js
@@ -1,32 +1,35 @@
 export const projectOverview = [
   {
     projectId: 1,
-    projectName: 'Games',
-    projectSrc: '',
-    projectDescription: "Games projects from odin's project. etch-a-Sketch, calculator, tic-tac-toe, Battleship."
+    projectName: "Games",
+    projectSrc: "/images/games.webp",
+    projectDescription:
+      "Games projects from odin's project. etch-a-Sketch, calculator, tic-tac-toe, Battleship.",
   },
   {
     projectId: 2,
-    projectName: 'Library',
-    projectSrc: '',
-    projectDescription: "Library to store the books that I've read, the rate I gave the book and my honest review."
+    projectName: "Library",
+    projectSrc: "",
+    projectDescription:
+      "Library to store the books that I've read, the rate I gave the book and my honest review.",
   },
   {
     projectId: 3,
-    projectName: 'Weather App',
-    projectSrc: '',
-    projectDescription: "App to look at my city weather and see the forecast."
+    projectName: "Weather App",
+    projectSrc: "",
+    projectDescription: "App to look at my city weather and see the forecast.",
   },
   {
     projectId: 4,
-    projectName: 'To do App',
-    projectSrc: '',
-    projectDescription: "App to track my taks and the things I have to do."
+    projectName: "To do App",
+    projectSrc: "",
+    projectDescription: "App to track my taks and the things I have to do.",
   },
   {
     projectId: 5,
-    projectName: 'CV Application',
-    projectSrc: '',
-    projectDescription: "Forms that help you build your own Resume to be ready to apply to jobs."
+    projectName: "CV Application",
+    projectSrc: "",
+    projectDescription:
+      "Forms that help you build your own Resume to be ready to apply to jobs.",
   },
-]
+];
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,27 +3,40 @@ import Home from './Pages/Home/Home'
 import Projects from "./Pages/Projects/Projects"
 import About from "./Pages/About/About"
 import Contact from "./Pages/Contact/Contact"
+import ProjectsCard from "./Pages/Projects/ProjectsCard/ProjectsCard"
+import Error from "./components/Error/Error"
 
 const routes = [
   {
     path: '/',
     element: <Root />,
+    errorElement: <Error/>,
     children: [
       {
         index: true,
-        element: <Home />
+        element: <Home />,
+        errorElement: <Error/>,
       },
       {
         path: 'projects',
-        element: <Projects/>
+        element: <Projects/>,
+        errorElement: <Error/>,
+        children: [
+          {
+            index: true,
+            element: <ProjectsCard color={'light'}/>
+          }
+        ]
       },
       {
         path: 'about',
-        element: <About/>
+        element: <About/>,
+        errorElement: <Error/>,
       },
       {
         path: 'contact',
-        element: <Contact/>
+        element: <Contact/>,
+        errorElement: <Error/>,
       }
     ]
   },
